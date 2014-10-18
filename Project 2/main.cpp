ce0857963f4a2bd4a83c617dca8a19dd1df2a89d
blackbbc <505968815@qq.com>
2014-10-18 18:27:35 +0800

    Add docs for project2 and project4

--- Project 2/main.cpp
+++ Project 2/main.cpp
@@ -8,9 +8,9 @@ class person
 {
 	friend game;
 private:
-	int num;
-	person *llink;
-	person *rlink;
+	int num;             //序号 
+	person *llink;       //左儿子 
+	person *rlink;       //右儿子 
 public:
 	person(int pos) :num(pos){}
 };
@@ -18,17 +18,17 @@ public:
 class game
 {
 private:
-	int n;
-	int m;
-	int s;
-	int k;
-	person *head;
+	int n;             //总人数 
+	int m;             //死亡数字 
+	int s;             //开始位置 
+	int k;             //剩余人数 
+	person *head;      //链表入口（头指针） 
 
 
 public:
-	void Initial();
-	void start();
-	void stop();
+	void Initial();    //初始化链表 
+	void start();      //开始游戏 
+	void stop();       //释放链表 
 
 };
 
@@ -39,7 +39,7 @@ int main()
 	liveOrDie.Initial();
 	liveOrDie.start();
 	liveOrDie.stop();
-
+	system("pause");
 }
 
 void game::Initial()
@@ -59,6 +59,7 @@ void game::Initial()
 	head = new person(1);
 	tail = head;
 
+	//建立双向链表+循环链表 
 	for (int i = 2; i <= n; i++)
 	{
 		p = new person(i);
@@ -67,7 +68,7 @@ void game::Initial()
 		p->rlink = NULL;
 		tail = p;
 	}
-
+	//将头指针与尾指针链接到一起 
 	tail->rlink = head;
 	head->llink = tail;
 
@@ -77,24 +78,27 @@ void game::start()
 {
 
 	int i, j;
-	person *p = head,*temp;
+	person *p = head, *temp;
 	s--;
 	if (s == 0)
 		s = n;
-
+	
+	//找到开始位置的前一个人，设为初始位置 
 	while (p->num != s)
 		p = p->rlink;
 
-	for (i = 0; i<n-k; i++)
+	for (i = 0; i<n - k; i++)
 	{
+		//数M个人 
 		for (j = 0; j<m; j++)
 			p = p->rlink;
-
-		cout << "第"<<i+1<<"个死者的位置是："<<p->num<<endl;
-
+		//输出序号 
+		cout << "第" << i + 1 << "个死者的位置是：" << p->num << endl;
+		
+		//如果删除的是头指针，则将头指针往后移一位 
 		if (head->num == p->num)
 			head = head->rlink;
-
+		//删除当前的P指针 
 		p->llink->rlink = p->rlink;
 		p->rlink->llink = p->llink;
 		temp = p;
@@ -105,8 +109,10 @@ void game::start()
 
 	cout << endl << "最后剩下：" << k << "人" << endl;
 	cout << "剩余的生者位置为：";
+	
+	//输出剩余人数 
 	p = head;
-	for (i = 0; i <  k; i++)
+	for (i = 0; i < k; i++)
 	{
 		cout << p->num << " ";
 		p = p->rlink;
--- Project 4/main.cpp
+++ Project 4/main.cpp
@@ -4,8 +4,10 @@
 using namespace std;
 
 int n, m, sum, upperlim;
-int ans[20];
+int ans[20];//记录状态，用来打印状态 
 
+
+//输出答案 
 void printAns()
 {
 	int i, j;
@@ -27,18 +29,23 @@ void printAns()
 	cout << endl;
 }
 
+//row、ld和rd，分别表示在纵列和两个对角线方向的限制条件下这一行的哪些地方不能放
 void test(int row, int ld, int rd)
 {
 	int pos, p;
-
+	
+	
 	if (row != upperlim)
 	{
+		//取出当前可以放的状态,用1表示可以放 
 		pos = upperlim&~(row | ld | rd);
 		while (pos)
 		{
+			//取出最右边的第一个1 
 			p = pos&-pos;
 			ans[m] = p;
 			m++;
+			//减去这个状态 
 			pos -= p;
 			test(row + p, (ld + p) << 1, (rd + p) >> 1);
 			m--;
@@ -46,8 +53,8 @@ void test(int row, int ld, int rd)
 
 	}
 	else
-	{
-		printAns();
+	{ 
+		printAns(); //打印答案 
 		sum++;
 	}
 
@@ -58,9 +65,14 @@ int main()
 	cout << "请输入皇后的个数：";
 	cin >> n;
 	m = 0;
+	
+	//先产生掩码，N个1，1表示不能放，0表示可以放 
 	upperlim = (1 << n) - 1;
 	cout <<endl<< "皇后摆法：" << endl<<endl;
+	
 	test(0, 0, 0);
+	
 	cout << "共有"<<sum<<"种解法！"<<endl;
+	system("pause");
 	return 0;
 }
