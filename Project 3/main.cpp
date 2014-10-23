4935fd4a7741fc867ba373158634351154ebc5f0
blackbbc <505968815@qq.com>
2014-10-23 08:34:49 +0800

    project 3

--- Project 3/main.cpp
+++ Project 3/main.cpp
@@ -8,11 +8,16 @@ struct point
     int y;
 };
 
-int n,m;
-char map[50][50];
+int n=7,m=7;
+char map[50][50]={"#######",
+				  "#0#0000",
+				  "#0#0###",
+				  "#000#0#",
+				  "#0#000#",
+				  "#0#0#0#",
+				  "#######"};
 point dir[4]={{-1,0},{0,1},{1,0},{0,-1}};
 point start,goal;
-bool flag=false;
 
 void getmap();
 bool dfs(int,int);
@@ -33,11 +38,8 @@ int main()
 
 void getmap()
 {
-    cin>>n>>m;
-    for (int i=0;i<n;i++)
-        for (int j=0;j<n;j++)
-            cin>>map[i][j];
-
+	start.x=1;start.y=1;
+	goal.x=5;goal.y=5;
 }
 
 
@@ -45,17 +47,20 @@ bool dfs(int x,int y)
 {
     if ((x==goal.x)&&(y=goal.y))
     {
-        flag=true;
+    	map[x][y]='x';
         return true;
     }
 
     for (int i=0;i<4;i++)
         if (map[x+dir[i].x][y+dir[i].y]=='0')
+        {
+        	map[x][y]='x';
             if (dfs(x+dir[i].x,y+dir[i].y))
-            {
-                map[x][y]='x';
                 return true;
-            }
+            map[x][y]='0';
+        	
+        }
+
 
     return false;
 
@@ -66,18 +71,19 @@ void printAns()
     int x=start.x;
     int y=start.y;
 
-    cout<<"<"<<x<<","<<y<<"> ---> ";
+    cout<<"<"<<x<<","<<y<<">";
 
-    while ((x!=goal.x)&&(y!=goal.y))
+    while ((x!=goal.x)||(y!=goal.y))
     {
         for (int i=0;i<4;i++)
             if (map[x+dir[i].x][y+dir[i].y]=='x')
             {
+            	map[x][y]='0';
                 x+=dir[i].x;
                 y+=dir[i].y;
                 break;
             }
 
-        cout<<"<"<<x<<","<<y<<"> ---> ";
+        cout<<" ---> "<<"<"<<x<<","<<y<<">";
     }
 }
