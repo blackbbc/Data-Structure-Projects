d0fe6a3f0e12c946ce262ec5832cd46e24c38639
blackbbc <505968815@qq.com>
2014-11-05 07:56:31 +0800

    Reverse

--- Project 6/main.cpp
+++ Project 6/main.cpp
@@ -1,225 +1,311 @@
 #include <iostream>
-#include <stack>
-#include <cctype>
-#include <cstring>
+#include <string>
 
 using namespace std;
 
-char a[100],*b;
-char pri[255];
-stack<char> exp;
-
-void initial()
+struct point
 {
-    pri['\0']=0;
-    pri['#']=0;
-    pri['^']=7;
-    pri['*']=5;
-    pri['/']=5;
-    pri['%']=5;
-    pri['+']=3;
-    pri['-']=3;
-    pri['(']=1;
-    pri[')']=1;
+	string name;
+	point *next;
+	point *child;
+	point *pre;
+	point *fa;
+};
+
+class family
+{
+	private:
+		point *head;
+	public:
+		family():head(NULL){}
+		~family();
+		void run();
+		void build();
+		point *fin(point *,string );
+		void ins(string );
+		void del(point *);
+		void dell(point *);
+		void mod(string );
+		void show(point *);
+};
+
+
+int main()
+{
+	family fam;
+	fam.run();
 }
 
-char *houzhui(char *ss,int n)
+family::~family()
 {
-    int i,j,m=0;
-    char t;
-    
-    n++;
-    char *a=new char[n];
-    strcpy(a,ss);
-    a[n-1]='#';
-
-    char *temp=new char[n];
-
-    exp.push('\0');
-
-    for (i=0;i<n;i++)
-    {
-        if (isdigit(a[i]))
-        {
-            temp[m]=a[i];
-            m++; 
-        }
-        else
-        {
-
-            if (a[i]=='(')
-                exp.push('(');
-            else
-                if (a[i]==')')
-                {
-                    while (exp.top()!='(')
-                    {
-                        temp[m]=exp.top();
-                        m++;
-                        exp.pop();
-                    }
-                    exp.pop();
-                }
-                else
-                {
-                    while ((!exp.empty())&&(pri[exp.top()]>=pri[a[i]]))
-                    {
-                        temp[m]=exp.top();
-                        m++;
-                        exp.pop();
-                    }
-                    exp.push(a[i]);
-                }
-        }
-    }
-    
-
-    return temp;
-
+	del(head);
 }
 
-char *qianzhui(char *ss,int n)
+void family::run()
 {
-    int i,j,m=0;
-    char swap;
-    char t;
-    
-    
-	n++;
-    char *temp=new char[n];
-    char *a=new char[n];
-    a[0]='#';
-    a[1]='\0';
-    strcat(a,ss);
-
-
-    for (i=n-1;i>=0;i--)
-    {
-        if (isdigit(a[i]))
-        {
-            temp[m]=a[i];
-            m++; 
-        }
-        else
-        {
-
-            if (a[i]==')')
-                exp.push(')');
-            else
-                if (a[i]=='(')
-                {
-                    while (exp.top()!=')')
-                    {
-                        temp[m]=exp.top();
-                        m++;
-                        exp.pop();
-                    }
-                    exp.pop();
-                }
-                else
-                {
-                    while ((!exp.empty())&&(pri[exp.top()]>pri[a[i]]))
-                    {
-                        temp[m]=exp.top();
-                        m++;
-                        exp.pop();
-                    }
-                    exp.push(a[i]);
-                }
-        }
-    }
-    
-    temp[m]='\0';
-    m++;
-    
-    for (i=0;i<m/2;i++)
-    {
-    	swap=temp[i];
-    	temp[i]=temp[m-i-2];
-    	temp[m-i-2]=swap;
-    }
-
-    return temp;
-
-}
-
-float countexp(char *ex)
-{
-	int i,j;
-	stack<float> ans;
-	int n=strlen(ex);
+	int opt;
+	string s;
+	point *p;
 	
-	float a,b,c;
+	build();
 	
-	for (i=0;i<n;i++)
+	while (true)
 	{
-		if (isdigit(ex[i]))
+		cout<<"请选择要执行的操作：";
+		cin>>opt;
+		switch (opt)
 		{
-			ans.push(ex[i]-'0');
-		}
-		else
-		{
-			switch (ex[i])
-			{
-				case '+':
-					b=ans.top();
-					ans.pop();
-					a=ans.top();
-					ans.pop();
-					c=a+b;
-					ans.push(c);
-					break;
-				case '-':;
-					b=ans.top();
-					ans.pop();
-					a=ans.top();
-					ans.pop();
-					c=a-b;
-					ans.push(c);
-					break;
-				case '*':;
-					b=ans.top();
-					ans.pop();
-					a=ans.top();
-					ans.pop();
-					c=a*b;
-					ans.push(c);
-					break;
-				case '/':;
-					b=ans.top();
-					ans.pop();
-					a=ans.top();
-					ans.pop();
-					c=a/b;
-					ans.push(c);
-					break;
-			}
+			//退出
+			case 0:
+				cout<<"感谢您的使用！"<<endl;
+				return; 
+			//查找子孙 
+			case 1:
+				cout<<"请输入要查找的人的姓名：";
+				cin>>s;
+				p=fin(head,s);
+				if (!p)
+					cout<<"查无此人！";
+				else
+					show(p);
+					
+				cout<<endl;
+				break;
+			//查找祖先 
+			case 2:
+				cout<<"请输入要查找的人的姓名：";
+				cin>>s;
+				p=fin(head,s);
+				if (!p)
+					cout<<"查无此人！";
+				else
+					cout<<s<<"的祖先是："<<p->fa->name<<endl;
+					
+				cout<<endl;
+				break;
+				
+			//插入 
+			case 3:
+				cout<<"请输入要插入到的家庭的祖先的姓名：";
+				cin>>s;
+				ins(s);
+				
+				
+				cout<<endl;
+				break;
+			//删除 
+			case 4:
+				cout<<"请输入要删除的人的姓名：";
+				cin>>s;
+				p=fin(head,s);
+				if (!p)
+					cout<<"查无此人！";
+				else
+				{
+					dell(p);
+				}
+				
+				cout<<endl;
+				break;
+			//修改 
+			case 5:
+				cout<<"请输入要修改的人的姓名：";
+				cin>>s;
+				mod(s);
+				
+				cout<<endl;
+				break;
+			default:
+				cout<<"输入错误！请重试"<<endl;
+				cout<<endl;
 		}
 		
 		
 	}
 	
-	return ans.top();
 	
 }
 
-
-
-
-int main()
+void family::build()
 {
+	string s;
+	cout<<"首先请建立祖先！"<<endl;
+	cout<<"请输入祖先的姓名：";
+	cin>>s;
+	head=new point();
+	head->name=s;
+	head->next=NULL;
+	head->child=NULL;
+	head->pre=NULL;
+	head->fa=head;
+}
 
-    initial();
-    cin>>a;
-    
-    b=houzhui(a,strlen(a));
-    cout<<b<<endl;
-    
-    cout<<countexp(b)<<endl;
-    
-    b=qianzhui(a,strlen(a));
-    cout<<b<<endl;
-    
+point *family::fin(point *p,string name)
+{
+	point *pp;
+	
+	//空指针 
+	if (!p)
+		return p;
+	//找到了 
+	if (p->name==name)
+		return p;
+	//找兄弟 
+	pp=fin(p->next,name);
+	if (pp)
+		return pp;
+	else
+		//找儿子 
+		return fin(p->child,name);
+	
+}
 
-    return 0;
+void family::ins(string name)
+{
+	int i,n;
+	string s;
+	point *tem;
+	point *pp;
+	point *p=fin(head,name);
+	if (!p)
+	{
+		cout<<"查无此人！"<<endl;
+		return;
+	}
+	
+	cout<<"请输入"<<p->name<<"的儿女个数：";
+	cin>>n;
+	cout<<"请输入"<<p->name<<"的儿女姓名（以空格分隔）：";
+	
+	//已经有儿子了 
+	if (p->child)
+	{
+		pp=p->child;
+		while (pp->next!=NULL)
+			pp=pp->next;
+		for (i=0;i<n;i++)
+		{
+			cin>>s;
+			tem=new point();
+			tem->name=s;
+			tem->next=NULL;
+			tem->child=NULL;
+			tem->fa=p;
+			tem->pre=pp;
+			pp->next=tem;
+			pp=pp->next;
+		}
+	}
+	//还没有儿子 
+	else
+	{
+		cin>>s;
+		tem=new point();
+		tem->name=s;
+		tem->next=NULL;
+		tem->pre=NULL;
+		tem->child=NULL;
+		tem->fa=p;
+		
+		p->child=tem;
+		pp=p->child;
+		
+		for (i=0;i<n-1;i++)
+		{
+			cin>>s;
+			tem=new point();
+			tem->name=s;
+			tem->next=NULL;
+			tem->child=NULL;
+			tem->fa=p;
+			tem->pre=pp;
+			pp->next=tem;
+			pp=pp->next;
+		}
+	}
+	
+	show(p);
+	
+}
+
+//把兄弟和儿子都删光光 
+void family::del(point *p)
+{
+	if (!p)
+		return;
+	del(p->child);
+	del(p->next);
+	delete p;
+	
+}
+
+//删除当前点并且连接前面的点 
+void family::dell(point *p)
+{
+	point *fa;
+	
+	//删除儿子 
+	del(p->child);
+	fa=p->fa;
+	
+	//当前是头 
+	if (p->pre==NULL)
+	{
+		//如果是独生子 
+		if (p->next==NULL)
+		{
+			fa->child=NULL;
+			
+		}
+		else
+		{
+			fa->child=p->next;
+		}
+	}
+	else
+		//当前是尾巴 
+		if (p->next==NULL)
+		{
+			p->pre->next=NULL;
+		}
+		else
+		{
+			p->pre->next=p->next;
+		}
+		
+	delete p;
+} 
+ 
+
+void family::mod(string name)
+{
+	string s;
+	point *p;
+	p=fin(head,name);
+	if (!p)
+	{
+		cout<<"查无此人！"<<endl;
+		return;
+	}
+	cout<<"请输入更改后的姓名：";
+	cin>>s;
+	p->name=s;
+}
+
+void family::show(point *src)
+{
+	point *p=src->child;
+	cout<<src->name<<"的第一代子孙是：";
+	if (!p)
+	{
+		cout<<"无！"<<endl;
+		return;
+	}
+
+	while (p)
+	{
+		cout<<p->name<<"   ";
+		p=p->next;
+	}
+	cout<<endl;
 }
--- Project 7/main.cpp
+++ Project 7/main.cpp
@@ -1,311 +1,234 @@
 #include <iostream>
-#include <string>
+#include <stack>
+#include <cctype>
+#include <cstring>
 
 using namespace std;
 
-struct point
-{
-	string name;
-	point *next;
-	point *child;
-	point *pre;
-	point *fa;
-};
+char a[100],*b;
+char pri[255];
+stack<char> exp;
 
-class family
+void initial()
 {
-	private:
-		point *head;
-	public:
-		family():head(NULL){}
-		~family();
-		void run();
-		void build();
-		point *fin(point *,string );
-		void ins(string );
-		void del(point *);
-		void dell(point *);
-		void mod(string );
-		void show(point *);
-};
+    pri['\0']=0;
+    pri['#']=0;
+    pri['^']=7;
+    pri['*']=5;
+    pri['/']=5;
+    pri['%']=5;
+    pri['+']=3;
+    pri['-']=3;
+    pri['(']=1;
+    pri[')']=1;
+}
+
+char *houzhui(char *ss,int n)
+{
+    int i,j,m=0;
+    char t;
+    
+    n++;
+    char *a=new char[n];
+    strcpy(a,ss);
+    a[n-1]='#';
+
+    char *temp=new char[n];
+
+    exp.push('\0');
+
+    for (i=0;i<n;i++)
+    {
+        if (isdigit(a[i]))
+        {
+            temp[m]=a[i];
+            m++; 
+        }
+        else
+        {
+
+            if (a[i]=='(')
+                exp.push('(');
+            else
+                if (a[i]==')')
+                {
+                    while (exp.top()!='(')
+                    {
+                        temp[m]=exp.top();
+                        m++;
+                        exp.pop();
+                    }
+                    exp.pop();
+                }
+                else
+                {
+                    while ((!exp.empty())&&(pri[exp.top()]>=pri[a[i]]))
+                    {
+                        temp[m]=exp.top();
+                        m++;
+                        exp.pop();
+                    }
+                    exp.push(a[i]);
+                }
+        }
+    }
+    
+
+    return temp;
+
+}
+
+char *qianzhui(char *ss,int n)
+{
+    int i,j,m=0;
+    char swap;
+    char t;
+    
+    
+	n++;
+    char *temp=new char[n];
+    char *a=new char[n];
+    a[0]='#';
+    a[1]='\0';
+    strcat(a,ss);
+
+
+    for (i=n-1;i>=0;i--)
+    {
+        if (isdigit(a[i]))
+        {
+            temp[m]=a[i];
+            m++; 
+        }
+        else
+        {
+
+            if (a[i]==')')
+                exp.push(')');
+            else
+                if (a[i]=='(')
+                {
+                    while (exp.top()!=')')
+                    {
+                        temp[m]=exp.top();
+                        m++;
+                        exp.pop();
+                    }
+                    exp.pop();
+                }
+                else
+                {
+                    while ((!exp.empty())&&(pri[exp.top()]>pri[a[i]]))
+                    {
+                        temp[m]=exp.top();
+                        m++;
+                        exp.pop();
+                    }
+                    exp.push(a[i]);
+                }
+        }
+    }
+    
+    temp[m]='\0';
+    m++;
+    
+    for (i=0;i<m/2;i++)
+    {
+    	swap=temp[i];
+    	temp[i]=temp[m-i-2];
+    	temp[m-i-2]=swap;
+    }
+
+    return temp;
+
+}
+
+float countexp(char *ex)
+{
+	int i,j;
+	stack<float> ans;
+	int n=strlen(ex);
+	
+	float a,b,c;
+	
+	for (i=0;i<n;i++)
+	{
+		if (isdigit(ex[i]))
+		{
+			ans.push(ex[i]-'0');
+		}
+		else
+		{
+			switch (ex[i])
+			{
+				case '+':
+					b=ans.top();
+					ans.pop();
+					a=ans.top();
+					ans.pop();
+					c=a+b;
+					ans.push(c);
+					break;
+				case '-':;
+					b=ans.top();
+					ans.pop();
+					a=ans.top();
+					ans.pop();
+					c=a-b;
+					ans.push(c);
+					break;
+				case '*':;
+					b=ans.top();
+					ans.pop();
+					a=ans.top();
+					ans.pop();
+					c=a*b;
+					ans.push(c);
+					break;
+				case '/':;
+					b=ans.top();
+					ans.pop();
+					a=ans.top();
+					ans.pop();
+					c=a/b;
+					ans.push(c);
+					break;
+			}
+		}
+		
+		
+	}
+	
+	return ans.top();
+	
+}
+
+
 
 
 int main()
 {
-	family fam;
-	fam.run();
-}
 
-family::~family()
-{
-	del(head);
-}
+    initial();
+    
+    cout<<"请输入表达式：";
+    cin>>a;
+    cout<<endl;
+    
+    cout<<"波兰表达式：";
+    b=qianzhui(a,strlen(a));
+    cout<<b<<endl<<endl;
+    
+    cout<<"中缀表达式：";
+    cout<<a<<endl<<endl;
+    
+    cout<<"逆波兰表达式：";
+	b=houzhui(a,strlen(a));
+    cout<<b<<endl<<endl;
+    
+    cout<<"表达式的值：";
+    cout<<countexp(b)<<endl<<endl;
+    
 
-void family::run()
-{
-	int opt;
-	string s;
-	point *p;
-	
-	build();
-	
-	while (true)
-	{
-		cout<<"请选择要执行的操作：";
-		cin>>opt;
-		switch (opt)
-		{
-			//退出
-			case 0:
-				cout<<"感谢您的使用！"<<endl;
-				return; 
-			//查找子孙 
-			case 1:
-				cout<<"请输入要查找的人的姓名：";
-				cin>>s;
-				p=fin(head,s);
-				if (!p)
-					cout<<"查无此人！";
-				else
-					show(p);
-					
-				cout<<endl;
-				break;
-			//查找祖先 
-			case 2:
-				cout<<"请输入要查找的人的姓名：";
-				cin>>s;
-				p=fin(head,s);
-				if (!p)
-					cout<<"查无此人！";
-				else
-					cout<<s<<"的祖先是："<<p->fa->name<<endl;
-					
-				cout<<endl;
-				break;
-				
-			//插入 
-			case 3:
-				cout<<"请输入要插入到的家庭的祖先的姓名：";
-				cin>>s;
-				ins(s);
-				
-				
-				cout<<endl;
-				break;
-			//删除 
-			case 4:
-				cout<<"请输入要删除的人的姓名：";
-				cin>>s;
-				p=fin(head,s);
-				if (!p)
-					cout<<"查无此人！";
-				else
-				{
-					dell(p);
-				}
-				
-				cout<<endl;
-				break;
-			//修改 
-			case 5:
-				cout<<"请输入要修改的人的姓名：";
-				cin>>s;
-				mod(s);
-				
-				cout<<endl;
-				break;
-			default:
-				cout<<"输入错误！请重试"<<endl;
-				cout<<endl;
-		}
-		
-		
-	}
-	
-	
-}
-
-void family::build()
-{
-	string s;
-	cout<<"首先请建立祖先！"<<endl;
-	cout<<"请输入祖先的姓名：";
-	cin>>s;
-	head=new point();
-	head->name=s;
-	head->next=NULL;
-	head->child=NULL;
-	head->pre=NULL;
-	head->fa=head;
-}
-
-point *family::fin(point *p,string name)
-{
-	point *pp;
-	
-	//空指针 
-	if (!p)
-		return p;
-	//找到了 
-	if (p->name==name)
-		return p;
-	//找兄弟 
-	pp=fin(p->next,name);
-	if (pp)
-		return pp;
-	else
-		//找儿子 
-		return fin(p->child,name);
-	
-}
-
-void family::ins(string name)
-{
-	int i,n;
-	string s;
-	point *tem;
-	point *pp;
-	point *p=fin(head,name);
-	if (!p)
-	{
-		cout<<"查无此人！"<<endl;
-		return;
-	}
-	
-	cout<<"请输入"<<p->name<<"的儿女个数：";
-	cin>>n;
-	cout<<"请输入"<<p->name<<"的儿女姓名（以空格分隔）：";
-	
-	//已经有儿子了 
-	if (p->child)
-	{
-		pp=p->child;
-		while (pp->next!=NULL)
-			pp=pp->next;
-		for (i=0;i<n;i++)
-		{
-			cin>>s;
-			tem=new point();
-			tem->name=s;
-			tem->next=NULL;
-			tem->child=NULL;
-			tem->fa=p;
-			tem->pre=pp;
-			pp->next=tem;
-			pp=pp->next;
-		}
-	}
-	//还没有儿子 
-	else
-	{
-		cin>>s;
-		tem=new point();
-		tem->name=s;
-		tem->next=NULL;
-		tem->pre=NULL;
-		tem->child=NULL;
-		tem->fa=p;
-		
-		p->child=tem;
-		pp=p->child;
-		
-		for (i=0;i<n-1;i++)
-		{
-			cin>>s;
-			tem=new point();
-			tem->name=s;
-			tem->next=NULL;
-			tem->child=NULL;
-			tem->fa=p;
-			tem->pre=pp;
-			pp->next=tem;
-			pp=pp->next;
-		}
-	}
-	
-	show(p);
-	
-}
-
-//把兄弟和儿子都删光光 
-void family::del(point *p)
-{
-	if (!p)
-		return;
-	del(p->child);
-	del(p->next);
-	delete p;
-	
-}
-
-//删除当前点并且连接前面的点 
-void family::dell(point *p)
-{
-	point *fa;
-	
-	//删除儿子 
-	del(p->child);
-	fa=p->fa;
-	
-	//当前是头 
-	if (p->pre==NULL)
-	{
-		//如果是独生子 
-		if (p->next==NULL)
-		{
-			fa->child=NULL;
-			
-		}
-		else
-		{
-			fa->child=p->next;
-		}
-	}
-	else
-		//当前是尾巴 
-		if (p->next==NULL)
-		{
-			p->pre->next=NULL;
-		}
-		else
-		{
-			p->pre->next=p->next;
-		}
-		
-	delete p;
-} 
- 
-
-void family::mod(string name)
-{
-	string s;
-	point *p;
-	p=fin(head,name);
-	if (!p)
-	{
-		cout<<"查无此人！"<<endl;
-		return;
-	}
-	cout<<"请输入更改后的姓名：";
-	cin>>s;
-	p->name=s;
-}
-
-void family::show(point *src)
-{
-	point *p=src->child;
-	cout<<src->name<<"的第一代子孙是：";
-	if (!p)
-	{
-		cout<<"无！"<<endl;
-		return;
-	}
-
-	while (p)
-	{
-		cout<<p->name<<"   ";
-		p=p->next;
-	}
-	cout<<endl;
+    return 0;
 }
